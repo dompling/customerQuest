a01b7351534c153003944261a528693ada0676d5
Lorenzo Maiuri <lorenzo.maiuri@ik.me>
2025-02-15 15:52:09 +0100

    Added game mode selection

--- lib/pages/play_pages/select_game_type_screen.dart
+++ lib/pages/play_pages/select_game_type_screen.dart
@@ -5,134 +5,332 @@ import 'package:flutter/material.dart';
 
 //------------------------------------------------------------------------------
 
-
-
 // SELECT GAME TYPE PAGE DEFINITION
-class SelectGameTypePage extends StatelessWidget  {
-
+class SelectGameTypePage extends StatelessWidget {
   // CLASS CONSTRUCTOR
   const SelectGameTypePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-
     // WIDGET CONTENT
     return Scaffold(
-
       // CENTER CONTENT
       body: SafeArea(
-
         child: Align(
-
           // INTERFACE ALIGNMENT
           alignment: Alignment.topCenter,
 
           // ALIGN CONTENT
           child: SingleChildScrollView(
-
             // SCROLLABLE CONTAINER CONTENT
             child: Container(
 
-              // SETTING CONTAINER PADDING
-              padding: EdgeInsets.all(15),
+                // SETTING CONTAINER PADDING
+                padding: EdgeInsets.all(10),
 
-              // CONTAINER CONTENT
-              child: Column(
+                // CONTAINER CONTENT
+                child: Column(
 
-               // COLUMN CONTENT
-               children: [
+                  // COLUMN CONTENT
+                  children: [
+                    //------------------------------------------------------------------------------
 
-                 //------------------------------------------------------------------------------
+                    // SPACER
+                    const SizedBox(height: 45),
 
-                 // SPACER
-                 const SizedBox(height: 45),
+                    //------------------------------------------------------------------------------
 
-                 //------------------------------------------------------------------------------
+                    // PAGE LOGO
+                    Image.asset(
+                      'images/select_game_mode_icon.png',
+                      width: 150,
+                      height: 150,
+                      fit: BoxFit.contain,
+                    ),
 
-                 // PAGE LOGO
-                 Image.asset(
-                   'images/player_selection_icon.png',
-                   width: 170,
-                   height: 170,
-                   fit: BoxFit.contain,
-                 ),
+                    //------------------------------------------------------------------------------
 
-                 //------------------------------------------------------------------------------
+                    // SPACER
+                    const SizedBox(height: 30),
 
-                 // SPACER
-                 const SizedBox(height: 30),
+                    //------------------------------------------------------------------------------
 
-                 //------------------------------------------------------------------------------
+                    // TITLE
+                    Text(
+                      // TEXT
+                      'Scegli la modalità di gioco',
 
-                 // TITLE
-                 Text(
+                      // TEXT STYLE
+                      style: TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
 
-                   // TEXT
-                   'Definisci i giocatori',
+                    //------------------------------------------------------------------------------
 
-                   // TEXT STYLE
-                   style: TextStyle(
+                    // SPACER
+                    const SizedBox(height: 30),
 
-                     fontSize: 29,
-                     fontWeight: FontWeight.bold,
+                    //------------------------------------------------------------------------------
 
-                   ),
+                    // FIRST GAME MODE BUTTON BOX
+                    SizedBox(
 
-                 ),
+                      //
+                      width: double.infinity,
 
-                 //------------------------------------------------------------------------------
+                      // FIRST GAME MODE BUTTON
+                      child: ElevatedButton(
 
-                 // SPACER
-                 const SizedBox(height: 30),
+                        //------------------------------------------------------------------------------
 
-                 //------------------------------------------------------------------------------
+                        // BUTTON STYLE PARAMETERS
+                        style: ButtonStyle(
 
-                 // PLAYER ONE CARD
-                 Card(
+                          // NORMAL TEXT COLOR
+                          foregroundColor: WidgetStateProperty.all(
+                              Theme.of(context).colorScheme.onPrimary
+                          ),
 
-                   // BACKGROUND COLOR
-                   color: Theme.of(context).colorScheme.primary,
+                          // NORMAL BACKGROUND COLOR
+                          backgroundColor: WidgetStateProperty.all(
+                            Theme.of(context).colorScheme.primary,
+                          ),
 
-                   // CARD CONTENT
-                   child: Column(
+                          // CORNERS RADIUS
+                          shape:
+                              WidgetStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
 
-                     // COLUMN CONTENT
-                     children: [
+                          // PADDING
+                          padding: WidgetStateProperty.all<EdgeInsets>(
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 25), // Padding interno
+                          ),
 
-                       // TITLE
-                       Text(
+                        ),
 
-                         // TEXT
-                         'Giocatore 1 :',
+                        //------------------------------------------------------------------------------
 
-                         // TEXT STYLE
-                         style: TextStyle(
+                        // ON PRESSED CALL
+                        onPressed: () {
+                          //print('Ciao ! Questa è una prova !');
+                        },
 
-                           fontSize: 20,
-                           fontWeight: FontWeight.normal,
+                        //------------------------------------------------------------------------------
 
-                         ),
+                        // BUTT0N CONTENT
+                        child: Row(
 
-                       ),
+                              // ROW CONTENT
+                              children: [
 
+                                //------------------------------------------------------------------------------
 
+                                // CARD ICON
+                                Image.asset(
+                                  'images/play_presence_icon.png',
+                                  width: 50,
+                                  height: 50,
+                                  fit: BoxFit.contain,
+                                ),
 
-                     ],
+                                //------------------------------------------------------------------------------
 
-                   ),
+                                // SPACER
+                                const SizedBox(width: 25),
 
-                 ),
+                                //------------------------------------------------------------------------------
 
-                 //------------------------------------------------------------------------------
+                                // COLUMN
+                                Expanded(
+                                  child: Column(
+                                    //
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
 
-                 // SPACER
-                 const SizedBox(height: 10),
+                                    children: [
+                                      // CARD TEXT
+                                      Text(
+                                        // TEXT
+                                        'Gioca in presenza',
 
-                 //------------------------------------------------------------------------------
+                                        // TEXT STYLE
+                                        style: TextStyle(
+                                          fontSize: 19,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
 
-               ],
+                                      // SPACER
+                                      const SizedBox(height: 3),
 
-              )
+                                      // CARD TEXT
+                                      Text(
+                                        // TEXT
+                                        'Vivi un eccitante avventura assieme al tuo partner giocando assieme di persona.',
+                                        maxLines: 3,
+                                        // TEXT STYLE
+                                        style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+
+                                    ],
+
+                                  ),
+
+                                ),
+
+                                //------------------------------------------------------------------------------
+                              ],
+
+                            ),
+
+                        //------------------------------------------------------------------------------
+
+                        ),
+
+                      ),
+
+                    //------------------------------------------------------------------------------
+
+                    // SPACER
+                    const SizedBox(height: 15),
+
+                    //------------------------------------------------------------------------------
+
+                    // SECOND GAME MODE BUTTON BOX
+                    SizedBox(
+
+                      //
+                      width: double.infinity,
+
+                      // FIRST GAME MODE BUTTON
+                      child: ElevatedButton(
+
+                        //------------------------------------------------------------------------------
+
+                        // BUTTON STYLE PARAMETERS
+                        style: ButtonStyle(
+
+                          // NORMAL TEXT COLOR
+                          foregroundColor: WidgetStateProperty.all(
+                              Theme.of(context).colorScheme.onPrimary
+                          ),
+
+                          // NORMAL BACKGROUND COLOR
+                          backgroundColor: WidgetStateProperty.all(
+                            Theme.of(context).colorScheme.primary,
+                          ),
+
+                          // CORNERS RADIUS
+                          shape:
+                          WidgetStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+
+                          // PADDING
+                          padding: WidgetStateProperty.all<EdgeInsets>(
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 25), // Padding interno
+                          ),
+
+                        ),
+
+                        //------------------------------------------------------------------------------
+
+                        // ON PRESSED CALL
+                        onPressed: () {
+                          //print('Ciao ! Questa è una prova !');
+                        },
+
+                        //------------------------------------------------------------------------------
+
+                        // BUTT0N CONTENT
+                        child: Row(
+
+                          // ROW CONTENT
+                          children: [
+
+                            //------------------------------------------------------------------------------
+
+                            // CARD ICON
+                            Image.asset(
+                              'images/play_distance_icon.png',
+                              width: 50,
+                              height: 50,
+                              fit: BoxFit.contain,
+                            ),
+
+                            //------------------------------------------------------------------------------
+
+                            // SPACER
+                            const SizedBox(width: 25),
+
+                            //------------------------------------------------------------------------------
+
+                            // COLUMN
+                            Expanded(
+                              child: Column(
+                                //
+                                crossAxisAlignment:
+                                CrossAxisAlignment.start,
+
+                                children: [
+                                  // CARD TEXT
+                                  Text(
+                                    // TEXT
+                                    'Gioca a distanza',
+
+                                    // TEXT STYLE
+                                    style: TextStyle(
+                                      fontSize: 19,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+
+                                  // SPACER
+                                  const SizedBox(height: 3),
+
+                                  // CARD TEXT
+                                  Text(
+                                    // TEXT
+                                    'Gioca assieme al tuo partner a distanza nella tua chat preferita e vivi magici momenti di coppia.',
+                                    maxLines: 3,
+                                    // TEXT STYLE
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+
+                                ],
+
+                              ),
+
+                            ),
+
+                            //------------------------------------------------------------------------------
+                          ],
+
+                        ),
+
+                        //------------------------------------------------------------------------------
+
+                      ),
+
+                    ),
+
+                    //------------------------------------------------------------------------------
+
+                  ],
+                )
 
             ),
 
