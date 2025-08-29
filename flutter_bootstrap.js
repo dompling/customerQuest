78a4e8925a4c903ebf676100d939aa0739f98fad
dompling <dompling@users.noreply.github.com>
2025-08-29 11:48:22 +0000

    deploy: 38eab4ea0c8ba04d887a5d950253d301fbd37c85

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "745980671"
+    serviceWorkerVersion: "817191159"
   }
 });
